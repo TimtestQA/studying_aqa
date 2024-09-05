d731b07dfee651364d7f39155ff8c33c5fe5139a
Timofey Kuznetsov <Freevoodoo@yandex.ru>
2024-09-06 00:32:35 +0300

    Домашнее задание 5 на тему "Условные операторы и вложенные операторы" 1 часть

--- Homework_5.py
+++ Homework_5.py
@@ -1,9 +1,28 @@
 num = int(input())
-if num > 0:
-    print('Число положительное')
-else:
-    print('Число отрицательное')
+print(positive_num = 'Число положительное' if num > 0 else 'Число отрицательное')
 
-if num < 0:
-    print('Число отрицательное')
-elif
+# if num < 0:
+#     print('Число отрицательное')
+# elif num = 0:
+#     print('Число равно нулю')
+num =  int(input())
+positive_num = 'Число положительное' if num > 0 else 'Число отрицательное'
+
+print(positive_num)
+
+if num > 0:
+    if 0 < num < 10:
+        print('Число от 1 до 9')
+    elif num >= 10:
+        print("Число 10 и больше")
+    elif num == 0:
+        print('Число равно нулю')
+
+is_raining = True
+is_sunny = True
+
+if is_raining and is_sunny:
+    print('дождь при солнце')
+
+is_raining = True
+is_sunny = True
