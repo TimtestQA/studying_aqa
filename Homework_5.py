f23ba2f126887bc8c786d00d6ecb5a976ed7f8d6
Timofey Kuznetsov <Freevoodoo@yandex.ru>
2024-09-05 23:30:32 +0300

    Домашнее задание 5 на тему "Условные операторы и вложенные операторы"

--- Homework_5.py
+++ Homework_5.py
@@ -1,3 +1,9 @@
-list1 = [[1] * 3] * 3
-list1[0][1] = 5
-print(list1[0][1])
+num = int(input())
+if num > 0:
+    print('Число положительное')
+else:
+    print('Число отрицательное')
+
+if num < 0:
+    print('Число отрицательное')
+elif
