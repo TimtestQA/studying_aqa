10d5468dbe3a82ca6dc74d9b30540b88593d55e0
timofeykuznetsov <freevoodoo@yandex.com>
2024-09-29 19:06:56 +0300

    Работа над ошибками 7 (вынес +=)

--- Homework_7.py
+++ Homework_7.py
@@ -3,7 +3,7 @@
 # Задание 1:
 # Создайте пустой список и с помощью цикла 'for' заполните его числами от 0 до 100 (range в помощь).
 from itertools import count
-
+#
 numerics_list = []
 for num in range(0,101):
     numerics_list.append(num)
@@ -21,9 +21,13 @@ print(numerics_list)
 # Задание 3:
 # Используйте цикл `while` и инструкцию `break` для поиска первого числа, которое делится на 7 без остатка,
 # в диапазоне от 1 до 100.
-total = 1
 
-while  1 <= total <100:
-        total +=1
-        if total // 7:
-            break
+total = 1
+while  1 <= total <= 100 :
+    if total // 7:
+        break
+    else:
+        print(total)
+        total += 1
+
+
