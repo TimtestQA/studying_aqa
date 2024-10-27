aea9e47373a5572636ccd31fd02699157396e6fc
Timofey Kuznetsov <Freevoodoo@yandex.ru>
2024-10-28 00:10:43 +0300

    Selenium Домашнее задание на тему "Работа с полями ввода и клавиатурой"

--- Selenium_homework/Selenium_homework3.py
+++ Selenium_homework/Selenium_homework3.py
@@ -9,56 +9,56 @@ UNIVERSAL_CTRL = Keys.COMMAND if os_name == 'darwin' else Keys.CONTROL
 
 driver = webdriver.Chrome()
 
-# MAIN_PAGE = driver.get("https://demoqa.com/text-box")
-#
-# user_name= driver.find_element("xpath", "//input[@id='userName']")
-# time.sleep(2)
-#
-# user_name.clear()
-#
-# assert user_name.get_attribute("value") == "","Поле не пустое"
-#
-# user_name.send_keys("John")
-#
-#
-# assert user_name.get_attribute("value") == "John", "Неверное имя пользователя"
-#
-#
-# email = driver.find_element ("xpath", "//input[@id='userEmail']")
-#
-# email.clear()
-#
-#
-# assert email.get_attribute("value")== "","Поле не пустое"
-#
-# email.send_keys("test.hobbit@gmail.com")
-#
-# assert email.get_attribute("value") == "test.hobbit@gmail.com", "Неверный email"
-#
-#
-# current_address = driver.find_element ("xpath", "//div/textarea[@id='currentAddress']")
-#
-# current_address.clear()
-#
-# assert current_address.get_attribute("value")== "","Поле не пустое"
-#
-# current_address.send_keys("Shire, Hobbiton, Bag End")
-#
-# assert current_address.get_attribute("value") == "Shire, Hobbiton, Bag End", "Адрес верный"
-#
-#
-# permanent_address = driver.find_element ("xpath", "//div/textarea[@id='permanentAddress']")
-#
-#
-# permanent_address.clear()
-#
-# assert permanent_address.get_attribute("value") == "", "Поле не пустое"
-#
-# permanent_address.send_keys("Middleeart")
-#
-# assert permanent_address.get_attribute("value") == "Middleeart"
-#
-# time.sleep(3)
+MAIN_PAGE = driver.get("https://demoqa.com/text-box")
+
+user_name= driver.find_element("xpath", "//input[@id='userName']")
+time.sleep(2)
+
+user_name.clear()
+
+assert user_name.get_attribute("value") == "","Поле не пустое"
+
+user_name.send_keys("John")
+
+
+assert user_name.get_attribute("value") == "John", "Неверное имя пользователя"
+
+
+email = driver.find_element ("xpath", "//input[@id='userEmail']")
+
+email.clear()
+
+
+assert email.get_attribute("value")== "","Поле не пустое"
+
+email.send_keys("test.hobbit@gmail.com")
+
+assert email.get_attribute("value") == "test.hobbit@gmail.com", "Неверный email"
+
+
+current_address = driver.find_element ("xpath", "//div/textarea[@id='currentAddress']")
+
+current_address.clear()
+
+assert current_address.get_attribute("value")== "","Поле не пустое"
+
+current_address.send_keys("Shire, Hobbiton, Bag End")
+
+assert current_address.get_attribute("value") == "Shire, Hobbiton, Bag End", "Адрес верный"
+
+
+permanent_address = driver.find_element ("xpath", "//div/textarea[@id='permanentAddress']")
+
+
+permanent_address.clear()
+
+assert permanent_address.get_attribute("value") == "", "Поле не пустое"
+
+permanent_address.send_keys("Middleeart")
+
+assert permanent_address.get_attribute("value") == "Middleeart"
+
+time.sleep(2)
 
 # Задание 2
 
